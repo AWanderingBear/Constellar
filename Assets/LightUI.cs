9d665cdce254f50f1cbb0bb3ad83701c4e394cc9
Josh Savage <yyrael@gmail.con>
2016-10-26 15:47:59 +1300

    Josh Testing particle systems

---
+++ Assets/LightUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightUI : MonoBehaviour {
+
+    public Transform Top;
+    public Transform Bottom;
+
+    private LineRenderer Bar;
+
+    Vector3[] BARPOS = new Vector3[2];
+
+    // Use this for initialization
+    void Start () {
+
+        Bar = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        BARPOS[0] = Top.position;
+        BARPOS[1] = Bottom.position;
+
+        Bar.SetPositions(BARPOS);
+	}
+}
