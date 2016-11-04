3895aa9261c613c47133481d7fe618cfdf730822
Anne Cho <miss.anne.c@hotmail.com>
2016-11-04 20:59:27 +1300

    Splash screen, scene manager, fading transition effect added.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    IEnumerator Fading()
+    {
+        float fadeTime = GameObject.Find("Game Manager").GetComponent<Fader>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+    }
+
+
+    public void SceneLoad(string SceneName)
+    {
+        Fading();
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
