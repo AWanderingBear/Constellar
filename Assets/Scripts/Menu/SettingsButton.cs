01f8961ac37f01461addeb843f753db4a91970ff
AWanderingBear <yyrael@gmail.com>
2016-11-10 23:32:15 +1300

    Buttons animating, extra art, hooks ready for buttons to work

---
+++ Assets/Scripts/Menu/SettingsButton.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeButton : MonoBehaviour {
+
+    public Sprite VolumeOn;
+    public Sprite VolumeOff;
+
+    bool VolumeActive = true;
+    bool Zoomed = false;
+    private SpriteRenderer Renderer;
+    // Use this for initialization
+    void Start () {
+
+        Renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+
+        SwapIcon();
+    }
+
+    void OnMouseOver()
+    {
+        if (!Zoomed)
+        {
+            Vector3 NewScale = transform.localScale;
+            NewScale.x += 0.1f;
+            NewScale.y += 0.1f;
+            transform.localScale = NewScale;
+            Zoomed = true; 
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (Zoomed)
+        {
+            Vector3 NewScale = transform.localScale;
+            NewScale.x -= 0.1f;
+            NewScale.y -= 0.1f;
+            transform.localScale = NewScale;
+            Zoomed = false;
+        }
+    }
+
+    public void SwapIcon()
+    {
+
+        if (VolumeActive)
+        {
+            Renderer.sprite = VolumeOff;
+            VolumeActive = false;
+        }
+        else
+        {
+            Renderer.sprite = VolumeOn;
+            VolumeActive = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/VolumeButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour {
+
+
+    bool Zoomed = false;
+    private SpriteRenderer Renderer;
+    // Use this for initialization
+    void Start () {
+
+        Renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+    }
+
+    void OnMouseOver()
+    {
+        if (!Zoomed)
+        {
+            Vector3 NewScale = transform.localScale;
+            NewScale.x += 0.1f;
+            NewScale.y += 0.1f;
+            transform.localScale = NewScale;
+            Zoomed = true; 
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (Zoomed)
+        {
+            Vector3 NewScale = transform.localScale;
+            NewScale.x -= 0.1f;
+            NewScale.y -= 0.1f;
+            transform.localScale = NewScale;
+            Zoomed = false;
+        }
+    }
+}
