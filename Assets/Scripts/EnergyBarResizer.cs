b2871afbcc784d1bd471d38e1300893e77431e9d
Amber <amberleejulian@gmail.com>
2016-11-16 15:01:37 +1300

    Needs to be combined with Annes level stuff. Also needs each level to have the canvas prefab placed in it.

---
+++ Assets/Scripts/EnergyBarResizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBarResizer : MonoBehaviour {
+
+
+    public StarManager starManager;
+    public float barPercentage;
+    // Use this for initialization
+    void Start () {
+     //   starManager = GameObject.Find("StarManager").GetComponent<StarManager>();             
+            }
+	
+	// Update is called once per frame
+	void Update () {
+        barPercentage = starManager.totalLevelEnergyPercentageUsed;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(barPercentage/2, 36.7f);
+
+        //max = 50.0 width means bar percentage needs to be divided by 2.
+        //Min = 0.0 width
+	}
+}
